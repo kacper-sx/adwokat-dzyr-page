6356a0ee263c19d3032816f6bde382ff2d33c3ec
kacper-sx <kacper.saweczko@o2.pl>
2025-04-29 14:11:50 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [icon({
+      include: {
+        lucide: [
+          "arrow-up-right",
+          "chevron-right",
+          "phone",
+          "menu",
+          "scroll",
+          "mail",
+          "hand-heart",
+          "crown",
+          "handshake",
+          "scale",
+          "map-pin",
+          "calendar-clock",
+          "hand-coins",
+          "calendar",
+          "heater",
+          "server",
+          'air-vent',
+          "battery-charging",
+          "cctv"
+        ],
+      },
+    }), sitemap()]
+});
---
+++ src/components/AppHeader/navData.ts
@@ -0,0 +1,8 @@
+
+
+export const navData = [
+  {href: "/#services", title: "Specjalności"},
+  {href: "/#about", title: "O mnie"},
+  {href: "/#collaboration", title: "Współpraca"},
+  {href: "/#contact", title: "Kontakt"},
+]
